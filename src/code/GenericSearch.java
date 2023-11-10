761c8f5d6c07713c9445fcfefa254d9f8a4aaac6
RobertoJoseph-Baraka <roberto@getbaraka.com>
2023-11-10 14:42:18 +0200

    change order of actions

--- src/code/GenericSearch.java
+++ src/code/GenericSearch.java
@@ -8,12 +8,13 @@ package code;
 
 
     public static void main(String[] args) {
-        String initialState = "72;" +
-            "36,13,35;" +
-            "75,96,62;" +
-            "20,2;5,2;33,2;" +
-            "30013,7,6,3,36;" +
-            "40050,5,10,14,44;";
+        String initialState =
+            "50;" +
+                "4,4,4;" +
+                "50,60,70;" +
+                "2,1;2,1;2,1;" +
+                "300,2,2,2,40;" +
+                "300,2,2,2,50;";
         String strategy = "DF";
         boolean visualize = true;
         LLAPSearch llapSearch = new LLAPSearch();
